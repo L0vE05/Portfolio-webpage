c0578492c6cd3fc9e5886bbdee4a15df3bda345b
Love Erlandsson <loveerla@playgroundsquad.com>
2026-03-03 17:03:48 +0100

    Neat footer

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,6 +1,6 @@
 
 const header = document.querySelector('.header')
-//const footer = document.querySelector('#footer')
+const footer = document.querySelector('.footer')
 
 window.addEventListener('load', function () {
     header.innerHTML = `
@@ -12,6 +12,26 @@ window.addEventListener('load', function () {
         <p class="navText">Home</p>
         <p class="navText">Projects</p>
         <p class="navText">About me</p>
+    </div>`
+
+    footer.innerHTML = `
+    <h2>Contact me</h2>
+    <div class="footerContactWrapper">
+        <div>
+            <div class="flex">
+                <p class="contactInfo">Mail:</p>
+                <p class="infoText">love.erlandsson@hotmail.com</p>
+            </div>
+            <div class="flex">
+                <p class="contactInfo">Phone:</p>
+                <p class="infoText">+46 (0)72 557 07 57</p>
+            </div>
+        </div>
+        <div class="contactLinks">
+            <img src="Assets/Icons/square-linkedin-brands-solid.png" class="linkIcon">
+            <img src="Assets/Icons/envelope-regular.png" class="linkIcon">
+            <img src="Assets/Icons/itch-io-brands-solid.png" class="linkIcon">
+        </div>
     </div>`
 })
 
